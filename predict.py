64b62921f2ea55cddd87ab207872f717a1e24d02
manhcuongk55 <dmcksclck55@gmail.com>
2019-12-18 14:14:07 +0700

    Add constant to predict

--- predict.py
+++ predict.py
@@ -7,16 +7,13 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision.models as models
-from torchvision import transforms
 
+from constant import HIDDEN_DIM, VAL_TRANSFORMS, PRETRAIN
 from model import CRNN
 
 print("start loading resnet50")
 resnet50 = models.resnet50(pretrained=True)
 resnet50.fc = nn.Linear(2048, 10)
-PRETRAIN = True
-HIDDEN_DIM = 512
-EPOCHS = 42
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -31,13 +28,6 @@ val_path = 'mnist_processed_images/validation_set/'
 image_files = []
 for file in glob(os.path.join(val_path, '*')):
     image_files.append(file)
-
-VAL_TRANSFORMS = transforms.Compose([
-    transforms.Pad((0, 26)),
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
 
 img = PIL.Image.open(image_files[12])
 plt.imshow(img)
